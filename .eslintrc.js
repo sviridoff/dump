c0bbe3ecd8b97cede5a5a1412040acb631f667f3
Alexander Sviridov <sviridoff.mail@gmail.com>
2021-12-27 19:37:15 +0100

    first

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+export default {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser.
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin.
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features.
+    sourceType: 'module', // Allows for the use of imports.
+  },
+  ignorePatterns: ['dist'],
+  rules: {
+    curly: 'warn',
+  },
+};
